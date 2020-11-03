38af25b582ed535dba31a8c3ca2fd43a1406603c
celipas <celipas@gmail.com>
2020-11-02 17:37:51 -0700

    Progress bar, CPU overload and notifions API

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,11 +4,29 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 18;
+
+//
+
 // Rune every 2 seconds
+notifyUser({
+  title: 'CPU Overload',
+  body: `CPU is over ${cpuOverload}%`,
+  icon: path.join(__dirname, 'img', 'icon.png'),
+});
 setInterval(() => {
   // CPU Usage
   cpu.usage().then(info => {
     document.getElementById('cpu-usage').innerText = info.toFixed(2) + '%';
+
+    document.getElementById('cpu-progress').style.width = info + '%';
+
+    // Make progress bar red if overload
+    if (info >= cpuOverload) {
+      document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+      document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
   });
 
   // CPU Free
@@ -37,11 +55,16 @@ mem.info().then(info => {
 
 // Show days, hours, mins, sec
 function secondsToDhms(seconds) {
-  console.log(seconds);
+  // console.log(seconds);
   seconds = +seconds;
   const d = Math.floor(seconds / (3600 * 24));
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
   return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+// Send notification
+function notifyUser(options) {
+  new Notification(options.title, options);
 }
